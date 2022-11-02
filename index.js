2655b2a53bc299517eb8be3f648058d3fa4d1949
Anna Harder <annaqharder@gmail.com>
2022-11-02 15:23:49 -0600

    commit tree 228812db83f808f8e39fd2c9ee0d7eeafec366ac

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+
+// function superbowlWin(collection){ 
+//     for (const record of collection) { 
+//         if(record.result === "W"){
+//             console.log(record.year)
+//         } else {
+//             console.log('undefined')
+//         }
+//     }
+// }
+// superbowlWin.find(record); 
+
+// function superbowlWin(record){ 
+//     for (const collection of record) { 
+//         if(record.result === "W"){
+//             console.log(record.year)
+//         } else {
+//             console.log('undefined')
+//         }
+//     }
+// }
+// superbowlWin(record); 
+
+
+// record.find(record.results === 'W')
+
+function superbowlWin(record) {
+    const result = record.find(record => record.result === 'W')
+    return !!result ? result.year : undefined
+}
+
+
